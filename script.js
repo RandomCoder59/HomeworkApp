84fefea4a9234daa68246c03b6aacda43ba6287a
RandomCoder59 <102413485+RandomCoder59@users.noreply.github.com>
2023-11-27 18:10:34 +0530

    Added all files...

---
+++ script.js
@@ -0,0 +1,121 @@
+getStringDate = (date) => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    if (month < 10) {
+        month = "0" + month;
+    }
+    let day = date.getDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    return year + "/" + month + "/" + day
+}
+
+getReverseStringDate = (date) => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    if (month < 10) {
+        month = "0" + month;
+    }
+    let day = date.getDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    return year + "/" + month + "/" + day
+}
+
+createTask = (subject, task) => {
+    return {subject, task};
+}
+
+createTaskElement = (task, date) => {
+    div = document.createElement("div");
+    div.className = "task";
+    p1 = document.createElement("p");
+    p1.className = "subject"
+    p2 = document.createElement("p");
+    p2.className = "content"
+    p1.innerText = task.subject + ":";
+    p2.innerText = task.task;
+    if (date) {
+        dateElement = document.createElement("p");
+        dateElement.className = "date"
+        dateElement.innerText = date + " - ";
+        div.appendChild(dateElement);
+    }
+    div.appendChild(p1);
+    div.appendChild(p2);
+    return div;
+}
+
+let url = "https://raw.githubusercontent.com/RandomCoder59/HomeworkJson/main/data.json";
+let data = {};
+let todayDate = getStringDate(new Date());
+let todayList = document.getElementById("today-tasks");
+let upcomingList = document.getElementById("upcoming-or-previous-tasks");
+let previousButton = document.getElementById("previous");
+let upcomingButton = document.getElementById("upcoming");
+let fitlerSelect = document.getElementById("filter")
+
+document.onload = () => {
+    let loaders = document.querySelectorAll(".loader");
+    for (loader of loaders) {
+        loader.remove();
+    }
+    todayDate = getStringDate(new Date());
+    todayTasks = data[todayDate];
+    if (todayTasks && todayTasks.length > 0) {
+        for (task of todayTasks) {
+            console.log(task);
+            todayList.appendChild(createTaskElement(task));
+        }
+    } else {
+        todayList.appendChild(createTaskElement(createTask("General", "Check upcoming tasks")))
+    }
+    upcomingButton.onchange();
+}
+
+previousButton.onchange = () => {
+    if (!previousButton.checked) {
+        return;
+    }
+    upcomingList.innerHTML = "";
+    for (date of Object.keys(data)) {
+        if (date < todayDate) {
+            for (task of data[date]) {
+                if (fitlerSelect.value == "All" || fitlerSelect.value == task.subject) {
+                    upcomingList.appendChild(createTaskElement(task, date));
+                }
+            }
+        }
+    }
+}
+
+upcomingButton.onchange = () => {
+    if (!upcomingButton.checked) {
+        return;
+    }
+    upcomingList.innerHTML = "";
+    for (date of Object.keys(data)) {
+        if (date > todayDate) {
+            for (task of data[date]) {
+                if (fitlerSelect.value == "All" || fitlerSelect.value == task.subject) {
+                    upcomingList.appendChild(createTaskElement(task, date));
+                }
+            }
+        }
+    }
+}
+
+fitlerSelect.onchange = () => {
+    previousButton.onchange();
+    upcomingButton.onchange();
+}
+
+fetch(url)
+.then(res => res.json())
+.then(out => {
+    data = out;
+    document.onload()
+})
+.catch(err => {alert("Unable to load tasks! Kindly refresh!"); document.reload();});
